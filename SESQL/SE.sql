2e4e92bc406754e324cca4bf5d902dd7597fdee2
thanhkhiem1411 <133770651+thanhkhiem1411@users.noreply.github.com>
2023-12-13 17:21:38 +0700

    Update SE.sql

--- SESQL/SE.sql
+++ SESQL/SE.sql
@@ -1,49 +1,53 @@
 Create Table Bank (
-    Branch_ID INT(20) AUTO_INCREMENT PRIMARY KEY,
+    Branch_ID INT AUTO_INCREMENT PRIMARY KEY,
     Address Varchar(255) NOT NULL,
-    Branch_City VARCHAR(50) NOT NULL,
+    Branch_City VARCHAR(255) NOT NULL,
     Branch_Name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Acc_types (
-  Acctype_ID INT(20) AUTO_INCREMENT PRIMARY KEY,
-  Account_Number INT(20) NOT NULL,
-  Name VARCHAR(50) NOT NULL,
+  Acctype_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Account_Number INT NOT NULL,
+  Name VARCHAR(255) NOT NULL,
   Description long text NOT NULL,
-  Rate Decimal(10, 2) NOT NULL,
+  Rate DECIMAL(10, 2) NOT NULL,
   FOREIGN KEY (Account_Number) REFERENCES BankAccounts(Account_Number)
 )
 
 CREATE TABLE BankAccounts (
-  Account_Number INT(20) AUTO_INCREMENT PRIMARY KEY,
-  Acc_Name VARCHAR(50) NOT NULL,
-  Acc_Status VARCHAR(200) NOT NULL,
-  Acc_Amount VARCHAR(200) NOT NULL,
-  Customer_ID INT(20) NOT NULL,
+  Account_Number INT AUTO_INCREMENT PRIMARY KEY,
+  Customer_ID INT NOT NULL,
+  Admin_ID INT NOT NULL,
+  Acc_Name VARCHAR(255) NOT NULL,
+  Acc_Status VARCHAR(255) NOT NULL,
+  Acc_Amount VARCHAR(255) NOT NULL,
+  Password VARCHAR(255) NOT NULL,  
+  Customer_ID INT NOT NULL,
   Created_At timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   FOREIGN KEY (Customer_ID) REFERENCES Customers(Customer_ID)
+  FOREIGN KEY (Admin_ID) REFERENCES Admins(Admin_ID)
 )
 
 CREATE TABLE Customers (
-  Customer_ID INT(2O) AUTO_INCREMENT PRIMARY KEY,
-  Cus_Name VARCHAR(200) NOT NULL,
-  National VARCHAR(200) NOT NULL,
-  Phone INT(15) NOT NULL,
+  Customer_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Cus_Name VARCHAR(255) NOT NULL,
+  National VARCHAR(255) NOT NULL,
+  Phone INT NOT NULL,
   Date_of_Birth DATE NOT NULL,
   Gender VARCHAR(10) NOT NULL,
-  Address VARCHAR(200) NOT NULL,
-  Email VARCHAR(200) NOT NULL,
-  Password VARCHAR(200) NOT NULL,
-  Profile_pic VARCHAR(200) NOT NULL,
+  Address VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  Profile_pic VARCHAR(255) NOT NULL,
 )  
 
-Create Table Admin (
-    Admin_ID INT IDENTITY (1,1) PRIMARY KEY,
-    Account_ID INT,
-    Branch_ID INT,
-    Name Varchar(255),
-    FOREIGN KEY (Branch_ID) REFERENCES Bank(Branch_ID),
-    FOREIGN KEY (Account_ID) REFERENCES Account(Account_ID),
+Create Table Admins (
+  Admin_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Branch_ID INT NOT NULL,
+  Name VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  Phone INT NOT NULL,
+  Profile_Pic VARCHAR(255) NOT NULL
+  FOREIGN KEY (Branch_ID) REFERENCES Bank(Branch_ID),
 );
 
 CREATE TABLE Loan (
