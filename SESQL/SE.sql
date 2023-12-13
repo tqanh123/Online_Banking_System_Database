e454dd3964a5d60d873f3478baeeba93d01c7518
thanhkhiem1411 <133770651+thanhkhiem1411@users.noreply.github.com>
2023-12-13 17:26:17 +0700

    Update SE.sql

--- SESQL/SE.sql
+++ SESQL/SE.sql
@@ -50,16 +50,11 @@ Create Table Admins (
   FOREIGN KEY (Branch_ID) REFERENCES Bank(Branch_ID),
 );
 
-CREATE TABLE Loan (
-    Loan_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Admin_ID INT,
-    Loan_Type VARCHAR(50),
-    Loan_Amount DECIMAL(10, 2),
-    Interest_Rate DECIMAL(5, 2),
-    Term INT,
-    Status VARCHAR(255),
-    FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID),
-);
+CREATE TABLE Notifications (
+  Notification_id INT AUTO_INCREMENT NOT NULL,
+  Notification_details TEXT NOT NULL,
+  Created_At timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+)
 
 CREATE TABLE Card (
     Card_ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -69,55 +64,13 @@ CREATE TABLE Card (
     FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID),
 );
 
-CREATE TABLE Credit (
-    Credit_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Admin_ID INT,
-    Credit_Type VARCHAR(50),
-    Credit_Amount DECIMAL(10, 2),
-    Credit_Total DECIMAL(10, 2),
-    Credit_Desc VARCHAR(255),
-    Status VARCHAR(255),
-    FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID),
-);
-
 CREATE TABLE `Transaction` (
     Transaction_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Customer_ID INT,
-    Amount DECIMAL(10, 2),
-    Transaction_Type VARCHAR(50),
-    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+    Customer_ID INT NOT NULL,
+    Amount DECIMAL(10, 2) NOT NULL,
+    Transaction_Type VARCHAR(50) NOT NULL,
+    FOREIGN KEY (Customer_ID) REFERENCES Customers(Customer_ID)
 );
 
-CREATE TABLE Deposit (
-    Deposit_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Customer_ID INT,
-    Deposit_Amount DECIMAL(10, 2),
-    Interest_Rate DECIMAL(5, 2),
-    Term INT,
-    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
-);
 
-CREATE TABLE CustomerLoan (
-    Customer_ID INT,
-    Loan_ID INT,
-    PRIMARY KEY (Customer_ID, Loan_ID),
-    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
-    FOREIGN KEY (Loan_ID) REFERENCES Loan(Loan_ID)
-);
-
-CREATE TABLE CustomerCard (
-    Customer_ID INT,
-    Card_ID INT,
-    PRIMARY KEY (Customer_ID, Card_ID),
-    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
-    FOREIGN KEY (Card_ID) REFERENCES Card(Card_ID)
-);
-
-CREATE TABLE CustomerCredit (
-    Customer_ID INT,
-    Credit_ID INT,
-    PRIMARY KEY (Customer_ID, Credit_ID),
-    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
-    FOREIGN KEY (Credit_ID) REFERENCES Credit(Credit_ID)
-);
 
