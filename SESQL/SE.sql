ea1fc085cd3f16075332022d955706b4bd3671d3
thanhkhiem1411 <133770651+thanhkhiem1411@users.noreply.github.com>
2023-12-21 15:35:15 +0700

    Update SE.sql

--- SESQL/SE.sql
+++ SESQL/SE.sql
@@ -39,15 +39,27 @@ CREATE TABLE Acc_types (
   Rate DECIMAL(10, 2) NOT NULL
 );
 
+CREATE TABLE LoanTypes (
+  LoanType_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(50) NOT NULL,
+  Description VARCHAR(255),
+  Rate DECIMAL(4, 2) NOT NULL,
+  Installment_Period VARCHAR(10) DEFAULT 'Month'
+);
+
 CREATE TABLE Loans (
   Loan_ID INT AUTO_INCREMENT PRIMARY KEY,
   Customer_ID INT NOT NULL,
+  LoanType_ID INT NOT NULL,
   Loan_Amount DECIMAL(10, 2) NOT NULL,
-  Interest_Rate DECIMAL(4, 2) NOT NULL,
   Loan_Term INT NOT NULL,
   Start_Date DATE NOT NULL,
-  Status VARCHAR(20) NOT NULL
+  Status VARCHAR(20) NOT NULL,
+  Installment BOOLEAN,
+  FOREIGN KEY Customer_ID REFERENCES Customers(Customer_ID),
+  FOREIGN KEY LoanType_ID REFERENCES LoanTypes(LoanType_ID)
 );
+
 
 CREATE TABLE Notifications (
   Notification_ID INT AUTO_INCREMENT PRIMARY KEY,
