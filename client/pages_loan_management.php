a3b868f52242b71025865825f5171731ea21c666
KarryOwn <minhkhoi306@gmail.com>
2024-01-03 14:06:00 +0700

    update loan

--- client/pages_loan_management.php
+++ client/pages_loan_management.php
@@ -8,7 +8,6 @@ $Customer_id = $_SESSION['Customer_id'];
 $loan_id = $_GET['loan_id'] ?? '';
 ?>
 
-
 <!DOCTYPE html>
 <html>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
@@ -22,92 +21,106 @@ $loan_id = $_GET['loan_id'] ?? '';
         <!-- Main Sidebar Container -->
         <?php include("dist/_partials/sidebar.php"); ?>
         <div class="content-wrapper">
-        <section class="content">
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-              </div>
-              <div class="card-body">
-              <form method='post' action='process_loan.php'>
-                            <input type='hidden' name='customer_id' value='<?php echo $Customer_id; ?>'>  
-                            <label for='bank_account'>Choose Bank Account:</label>
-                            <select name='bank_account' id='bank_account'>
-                                <?php
-                                $sql = "SELECT * FROM bankaccounts WHERE Customer_ID = $Customer_id";
-                                $result = $mysqli->query($sql);
-                                
-                                if ($result->num_rows > 0) {
-                                    while ($row = $result->fetch_object()) {
-                                        echo "<option value='" . $row->Account_ID . "'>";
-                                        echo $row->Acc_Name . " - " . $row->Acc_Status . " (" . $row->Acc_Amount . ")";
-                                        echo "</option>";
+            <section class="content">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                            </div>
+                            <div class="card-body">
+                            <form method='post' action='pages_loan_process.php' id='checkoutForm'>
+                                <input type='hidden' name='customer_id' value='<?php echo $Customer_id; ?>'>  
+                                <label for='loan_account'>Choose Loan Account:</label>
+                                <select name='loan_account' id='loan_account'>
+                                    <?php
+                                    $sql = "SELECT * FROM loans WHERE Customer_ID = $Customer_id";
+                                    $result = $mysqli->query($sql);
+
+                                    if ($result->num_rows > 0) {
+                                        while ($row = $result->fetch_object()) {
+                                            echo "<option value='" . $row->Loan_ID . "'>";
+                                            echo  " - " . $row->Status . " (" . $row->Loan_Amount . ")";
+                                            echo "</option>";
+                                        }
+                                    } else {
+                                        echo "<option value=''>No loan accounts available</option>";
                                     }
-                                } else {
-                                    echo "<option value=''>No bank accounts available</option>";
-                                }
-                                ?>
-                            </select><br><br>
-                            <form method='post' action='process_loan.php' id='checkoutForm'>
-                                <input type='hidden' name='customer_id' value='<?php echo $row->Customer_ID; ?>'>
+                                    ?>
+                                </select><br><br>
+                                <label for='bank_account'>Choose Bank Account:</label>
+                                <select name='bank_account' id='bank_account'>
+                                    <?php
+                                    $sqlBank = "SELECT * FROM BankAccounts WHERE Customer_ID = $Customer_id";
+                                    $resultBank = $mysqli->query($sqlBank);
+
+                                    if ($resultBank->num_rows > 0) {
+                                        while ($rowBank = $resultBank->fetch_object()) {
+                                            echo "<option value='" . $rowBank->Account_Number . "'>";
+                                            echo  $rowBank->Account_Number . " - " . $rowBank->Acc_Status . " (" . $rowBank->Acc_Amount . ")";
+                                            echo "</option>";
+                                        }
+                                    } else {
+                                        echo "<option value=''>No bank accounts available</option>";
+                                    }
+                                    ?>
+                                </select><br><br>
+                                <?php if(isset($loan_id)) { ?>
+                                    <input type='hidden' name='loan_id' value='<?php echo $loan_id; ?>'>
+                                <?php } ?>
                                 <input type='hidden' name='loan_action' value='accept'>
-                                <button type='button' onclick='submitForm()' class='btn btn-primary'>Checkout</button>
+                                <button type='submit' class='btn btn-primary'>Checkout</button>
                             </form>
-                        </form>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </div>
-          <!-- /.col -->
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </section>
+            <!-- /.content -->
         </div>
-        <!-- /.row -->
-      </section>
-      <!-- /.content -->
+        <!-- /.content-wrapper -->
+        <?php include("dist/_partials/footer.php"); ?>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
-    <!-- /.content-wrapper -->
-    <?php include("dist/_partials/footer.php"); ?>
+    <!-- ./wrapper -->
 
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-  </div>
-  <!-- ./wrapper -->
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- DataTables -->
+    <script src="plugins/datatables/jquery.dataTables.js"></script>
+    <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="dist/js/demo.js"></script>
+    <!-- page script -->
+    <script>
+        $(function() {
+            $("#example1").DataTable();
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+            });
+        });
 
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- DataTables -->
-  <script src="plugins/datatables/jquery.dataTables.js"></script>
-  <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
-  <!-- AdminLTE for demo purposes -->
-  <script src="dist/js/demo.js"></script>
-  <!-- page script -->
-  <script>
-    $(function() {
-      $("#example1").DataTable();
-      $('#example2').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-      });
-    });
-  </script>
-
-<script>
-    function submitForm() {
-        document.getElementById('checkoutForm').submit();
-    }
-</script>
-
+        function submitForm() {
+            document.getElementById('checkoutForm').submit();
+        }
+    </script>
 </body>
 
 </html>
